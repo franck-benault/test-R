06128a8337218577e6eb6e8ae383d00ca78dd492
franck-benault <franck-benault@orange.fr>
2015-08-15 23:33:31 +0200

    add usage of matrix

--- s00datatype/s00datatype.R
+++ s00datatype/s00datatype.R
@@ -1,4 +1,4 @@
-#simple variable
+#simple variable scalar
 a <- 1
 b <- "a String"
 
@@ -12,3 +12,27 @@ c <- c(TRUE,TRUE,TRUE,FALSE,TRUE,FALSE) #logical vector
 #always the same type
 # here 1 becomes the string "1"
 d <- c(1,"a string")
+# usage
+d[2]
+
+########
+# matrix
+# it is limitated in two dimensions
+########
+# generates 5 x 4 numeric matrix
+# default column V1, V2 ... 
+y<-matrix(1:20, nrow=5,ncol=4)
+
+# another example
+cells <- c(1,26,24,68)
+rnames <- c("R1", "R2")
+cnames <- c("C1", "C2")
+mymatrix <- matrix(cells, nrow=2, ncol=2, byrow=TRUE,
+                   dimnames=list(rnames, cnames))
+#usage of the matrix
+mymatrix["R1","C1"]
+#
+mymatrix[,2] # 2nd column of matrix
+mymatrix[1,] # 1st row of matrix
+mymatrix[1,1:2] # rows 1 of columns 1,2 
+
