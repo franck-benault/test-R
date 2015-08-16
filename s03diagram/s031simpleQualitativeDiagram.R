aa06628779f2480f739ace7acfd6bfd8f4de95da
franck-benault <franck-benault@orange.fr>
2015-08-16 23:10:43 +0200

    complete pie diagrams

--- s03diagram/s031simpleQualitativeDiagram.R
+++ s03diagram/s031simpleQualitativeDiagram.R
@@ -1,12 +1,16 @@
 GCType <- c("Full", "Young", "Full", "Full","Young",
             "Young","Young", "Full", "Young", "Young",
             "Full","Young","Young","Young","Full")
+
+#Simple analysis data
 table(GCType)
 
-############
-#pie diagram
-############
+#############
+#Pie diagrams
+#############
+#Simple pie diagram
+lbls <- c("Full GC", "Young GC")
+pie(table(GCType), labels = lbls, main="Comparison Full Young GC")
+
+
 barplot(table(GCType))
-pie(table(GCType))
-
-
