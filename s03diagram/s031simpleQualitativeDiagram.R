ca3d64e2842eeace906f1e996a61168df32029af
franck-benault <franck-benault@orange.fr>
2015-08-16 23:21:37 +0200

    complete pie diagrams

--- s03diagram/s031simpleQualitativeDiagram.R
+++ s03diagram/s031simpleQualitativeDiagram.R
@@ -3,14 +3,22 @@ GCType <- c("Full", "Young", "Full", "Full","Young",
             "Full","Young","Young","Young","Full")
 
 #Simple analysis data
-table(GCType)
+resGCType <- table(GCType)
 
 #############
 #Pie diagrams
 #############
 #Simple pie diagram
 lbls <- c("Full GC", "Young GC")
-pie(table(GCType), labels = lbls, main="Comparison Full Young GC")
+mainLabel = "Comparison Full Young GC"
+pie(resGCType, labels = lbls, main=mainLabel)
+
+# Pie Chart with Percentages
+pct <- round(resGCType/sum(resGCType)*100)
+lbls <- paste(lbls, pct) # add percents to labels
+lbls <- paste(lbls,"%",sep="") # ad % to labels
+pie(resGCType,labels = lbls, col=rainbow(length(lbls)),
+    main=mainLabel) 
 
 
 barplot(table(GCType))
