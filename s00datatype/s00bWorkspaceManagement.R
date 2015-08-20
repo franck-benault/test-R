ba51e019424356299a693f1dd3e98ef2a3a6a03f
franck-benault <franck-benault@orange.fr>
2015-08-21 00:15:21 +0200

    add vector datatype

--- s00datatype/s00bWorkspaceManagement.R
+++ s00datatype/s00bWorkspaceManagement.R
@@ -6,40 +6,12 @@ v2 <- FALSE
 v3 <- T
 v4 <- F
 v5 <- NA
-class(v5)
-is.logical(v5)
-v6 <- as.logical("FALSE")
-v6
 
-#########
-# Numeric
-#########
-v1 <- 2
+#list of variables
+ls()
+#type of V1
 class(v1)
-is.numeric(v1)
-v2 <- as.numeric("4.5")
-
-#########
-# Integer
-#########
-v1 <- 2L
-class(v1)
-is.integer(v1)
-v2 <- as.integer("2")
-
-######################
-# others numeric types
-# double
-# complex ?
-######################
-
-##########
-#Character
-##########
-v1 <- "string"
-class(v1)
-is.character(v1)
-
-######################
-#Raw (store raw bytes)
-######################
+#remove v1 from workspace
+rm(v1)
+#list of variable without v1
+ls()
--- s00datatype/s00ddatatype.R
+++ s00datatype/s00ddatatype.R
@@ -1,20 +1,3 @@
-#simple variable scalar
-a <- 1
-b <- "a String"
-
-#########
-#Vectors
-#########
-a <- c(1,2,5.3,6,-2,4) # numeric vector
-b <- c("one","two","three") # character vector
-c <- c(TRUE,TRUE,TRUE,FALSE,TRUE,FALSE) #logical vector
-
-#always the same type
-# here 1 becomes the string "1"
-d <- c(1,"a string")
-# usage
-d[2]
-
 ########
 # matrix
 # it is limitated in two dimensions
