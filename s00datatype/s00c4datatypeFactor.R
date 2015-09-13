9e8be4527a2803779d0badf872d95d684ea15502
franck-benault <franck-benault@orange.fr>
2015-09-13 22:56:59 +0200

    add example with factors

--- s00datatype/s00c4datatypeFactor.R
+++ s00datatype/s00c4datatypeFactor.R
@@ -1,3 +1,14 @@
-###################
+############################################
 # data type factor
-###################
+# factor list of limited values (categories)
+# like in java enumeration
+############################################
+
+###ordered factor
+speed_vector <- c("Fast", "Slow", "Slow", "Fast", "Ultra-fast") 
+# Add your code below
+factor_speed_vector <- factor(speed_vector,ordered=TRUE, levels=c("Slow","Fast","Ultra-fast"))
+#summary
+summary(speed_vector)
+#we can compare the value of factor_speed_vector
+compare_them <- factor_speed_vector[2]>factor_speed_vector[5]
