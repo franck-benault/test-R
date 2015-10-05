33e04c70d7bf3938d78601eb04de5a48de9eade4
franck-benault <franck-benault@orange.fr>
2015-10-06 00:45:10 +0200

    complete example with lists

--- s00datatype/s00c6datatypeList.R
+++ s00datatype/s00c6datatypeList.R
@@ -1,6 +1,20 @@
-################
+#######################################################
 # data type list
 # ordered collection 
 # datatype of the elements can have different datatypes 
 # also can be vector, matrix dataframe
+# loss of some functionality
 #######################################################
+song <- list("Rsome types", 190, 5)
+song
+
+############
+#name a list
+############
+names(song) <- c("title","duration","track")
+song
+
+song <- list(title="Rsome types", duration=190, track=5)
+song
+#display a list
+str(song)
