8078d0386c5cc6b9b2f7d97312990f6cf114e5fe
franck-benault <franck-benault@orange.fr>
2015-10-06 01:07:36 +0200

    complete example dataframe

--- s00datatype/s00c5datatypeDataFrame.R
+++ s00datatype/s00c5datatypeDataFrame.R
@@ -2,6 +2,7 @@
 # data type dataframe
 # two dimensions, in a column all elements have the same datatype
 #####################
+people <-read.csv("people.csv", sep="", header=TRUE)
 #head(...) begin of the dataframe
 #tail(...) end of the dataframe
 
